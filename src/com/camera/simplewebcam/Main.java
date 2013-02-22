ec1da9d87bb4a5e8d5157f599905a5a2fa06ad44
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-02-22 16:48:47 -0500

    Replace tabs with 4 spaces.

--- src/com/camera/simplewebcam/Main.java
+++ src/com/camera/simplewebcam/Main.java
@@ -4,15 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Main extends Activity {
-	
-	CameraPreview cp;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-		cp = (CameraPreview) findViewById(R.id.cp); 
-	}
-	
+    
+    CameraPreview cp;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        cp = (CameraPreview) findViewById(R.id.cp); 
+    }
+    
 }
