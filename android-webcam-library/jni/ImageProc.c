b4859c04026680ae22c2a7fa335cbe5d21379958
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-02-23 13:47:42 -0500

    Clean up style in JNI interface.

--- android-webcam-library/jni/ImageProc.c
+++ android-webcam-library/jni/ImageProc.c
@@ -1,115 +1,112 @@
 #include "ImageProc.h"
 
-int errnoexit(const char *s)
-{
-    LOGE("%s error %d, %s", s, errno, strerror (errno));
+int errnoexit(const char *s) {
+    LOGE("%s error %d, %s", s, errno, strerror(errno));
     return ERROR_LOCAL;
 }
 
 
-int xioctl(int fd, int request, void *arg)
-{
+int xioctl(int fd, int request, void *arg) {
     int r;
 
-    do r = ioctl (fd, request, arg);
-    while (-1 == r && EINTR == errno);
+    do {
+        r = ioctl(fd, request, arg);
+    } while(-1 == r && EINTR == errno);
 
     return r;
 }
-int checkCamerabase(void){
+
+int checkCamerabase(void) {
     struct stat st;
     int i;
     int start_from_4 = 1;
 
     /* if /dev/video[0-3] exist, camerabase=4, otherwise, camrerabase = 0 */
-    for(i=0 ; i<4 ; i++){
+    for(i=0 ; i<4 ; i++) {
         sprintf(dev_name,"/dev/video%d",i);
-        if (-1 == stat (dev_name, &st)) {
+        if(-1 == stat(dev_name, &st)) {
             start_from_4 &= 0;
-        }else{
+        } else {
             start_from_4 &= 1;
         }
     }
 
-    if(start_from_4){
+    if(start_from_4) {
         return 4;
-    }else{
+    } else {
         return 0;
     }
 }
 
-int opendevice(int i)
-{
+int opendevice(int i) {
     struct stat st;
 
     sprintf(dev_name,"/dev/video%d",i);
 
-    if (-1 == stat (dev_name, &st)) {
-        LOGE("Cannot identify '%s': %d, %s", dev_name, errno, strerror (errno));
+    if(-1 == stat(dev_name, &st)) {
+        LOGE("Cannot identify '%s': %d, %s", dev_name, errno, strerror(errno));
         return ERROR_LOCAL;
     }
 
-    if (!S_ISCHR (st.st_mode)) {
+    if(!S_ISCHR(st.st_mode)) {
         LOGE("%s is no device", dev_name);
         return ERROR_LOCAL;
     }
 
-    fd = open (dev_name, O_RDWR | O_NONBLOCK, 0);
+    fd = open(dev_name, O_RDWR | O_NONBLOCK, 0);
 
-    if (-1 == fd) {
-        LOGE("Cannot open '%s': %d, %s", dev_name, errno, strerror (errno));
+    if(-1 == fd) {
+        LOGE("Cannot open '%s': %d, %s", dev_name, errno, strerror(errno));
         return ERROR_LOCAL;
     }
     return SUCCESS_LOCAL;
 }
 
-int initdevice(void)
-{
+int initdevice(void) {
     struct v4l2_capability cap;
     struct v4l2_cropcap cropcap;
     struct v4l2_crop crop;
     struct v4l2_format fmt;
     unsigned int min;
 
-    if (-1 == xioctl (fd, VIDIOC_QUERYCAP, &cap)) {
-        if (EINVAL == errno) {
+    if(-1 == xioctl(fd, VIDIOC_QUERYCAP, &cap)) {
+        if(EINVAL == errno) {
             LOGE("%s is no V4L2 device", dev_name);
             return ERROR_LOCAL;
         } else {
-            return errnoexit ("VIDIOC_QUERYCAP");
+            return errnoexit("VIDIOC_QUERYCAP");
         }
     }
 
-    if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
+    if(!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
         LOGE("%s is no video capture device", dev_name);
         return ERROR_LOCAL;
     }
 
-    if (!(cap.capabilities & V4L2_CAP_STREAMING)) {
+    if(!(cap.capabilities & V4L2_CAP_STREAMING)) {
         LOGE("%s does not support streaming i/o", dev_name);
         return ERROR_LOCAL;
     }
 
-    CLEAR (cropcap);
+    CLEAR(cropcap);
 
     cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 
-    if (0 == xioctl (fd, VIDIOC_CROPCAP, &cropcap)) {
+    if(0 == xioctl(fd, VIDIOC_CROPCAP, &cropcap)) {
         crop.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         crop.c = cropcap.defrect;
 
-        if (-1 == xioctl (fd, VIDIOC_S_CROP, &crop)) {
-            switch (errno) {
+        if(-1 == xioctl(fd, VIDIOC_S_CROP, &crop)) {
+            switch(errno) {
                 case EINVAL:
                     break;
                 default:
                     break;
             }
         }
-    } else {
     }
 
-    CLEAR (fmt);
+    CLEAR(fmt);
 
     fmt.type                = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 
@@ -119,133 +116,129 @@ int initdevice(void)
     fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
     fmt.fmt.pix.field       = V4L2_FIELD_INTERLACED;
 
-    if (-1 == xioctl (fd, VIDIOC_S_FMT, &fmt))
-        return errnoexit ("VIDIOC_S_FMT");
+    if(-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
+        return errnoexit("VIDIOC_S_FMT");
 
     min = fmt.fmt.pix.width * 2;
-    if (fmt.fmt.pix.bytesperline < min)
+    if(fmt.fmt.pix.bytesperline < min)
         fmt.fmt.pix.bytesperline = min;
     min = fmt.fmt.pix.bytesperline * fmt.fmt.pix.height;
-    if (fmt.fmt.pix.sizeimage < min)
+    if(fmt.fmt.pix.sizeimage < min)
         fmt.fmt.pix.sizeimage = min;
 
-    return initmmap ();
-
+    return initmmap();
 }
 
-int initmmap(void)
-{
+int initmmap(void) {
     struct v4l2_requestbuffers req;
 
-    CLEAR (req);
+    CLEAR(req);
 
     req.count               = 4;
     req.type                = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     req.memory              = V4L2_MEMORY_MMAP;
 
-    if (-1 == xioctl (fd, VIDIOC_REQBUFS, &req)) {
-        if (EINVAL == errno) {
+    if(-1 == xioctl(fd, VIDIOC_REQBUFS, &req)) {
+        if(EINVAL == errno) {
             LOGE("%s does not support memory mapping", dev_name);
             return ERROR_LOCAL;
         } else {
-            return errnoexit ("VIDIOC_REQBUFS");
+            return errnoexit("VIDIOC_REQBUFS");
         }
     }
 
-    if (req.count < 2) {
+    if(req.count < 2) {
         LOGE("Insufficient buffer memory on %s", dev_name);
         return ERROR_LOCAL;
-     }
+    }
 
-    buffers = calloc (req.count, sizeof (*buffers));
+    buffers = calloc(req.count, sizeof(*buffers));
 
-    if (!buffers) {
+    if(!buffers) {
         LOGE("Out of memory");
         return ERROR_LOCAL;
     }
 
-    for (n_buffers = 0; n_buffers < req.count; ++n_buffers) {
+    for(n_buffers = 0; n_buffers < req.count; ++n_buffers) {
         struct v4l2_buffer buf;
 
-         CLEAR (buf);
+        CLEAR(buf);
 
         buf.type        = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         buf.memory      = V4L2_MEMORY_MMAP;
         buf.index       = n_buffers;
 
-        if (-1 == xioctl (fd, VIDIOC_QUERYBUF, &buf))
-            return errnoexit ("VIDIOC_QUERYBUF");
+        if(-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf))
+            return errnoexit("VIDIOC_QUERYBUF");
 
         buffers[n_buffers].length = buf.length;
         buffers[n_buffers].start =
-        mmap (NULL ,
-            buf.length,
-            PROT_READ | PROT_WRITE,
-            MAP_SHARED,
-            fd, buf.m.offset);
+            mmap(NULL ,
+                    buf.length,
+                    PROT_READ | PROT_WRITE,
+                    MAP_SHARED,
+                    fd, buf.m.offset);
 
-        if (MAP_FAILED == buffers[n_buffers].start)
-            return errnoexit ("mmap");
+        if(MAP_FAILED == buffers[n_buffers].start)
+            return errnoexit("mmap");
     }
 
     return SUCCESS_LOCAL;
 }
 
-int startcapturing(void)
-{
+int startcapturing(void) {
     unsigned int i;
     enum v4l2_buf_type type;
 
-    for (i = 0; i < n_buffers; ++i) {
+    for(i = 0; i < n_buffers; ++i) {
         struct v4l2_buffer buf;
 
-        CLEAR (buf);
+        CLEAR(buf);
 
         buf.type        = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         buf.memory      = V4L2_MEMORY_MMAP;
         buf.index       = i;
 
-        if (-1 == xioctl (fd, VIDIOC_QBUF, &buf))
-            return errnoexit ("VIDIOC_QBUF");
+        if(-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+            return errnoexit("VIDIOC_QBUF");
     }
 
     type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 
-    if (-1 == xioctl (fd, VIDIOC_STREAMON, &type))
-        return errnoexit ("VIDIOC_STREAMON");
+    if(-1 == xioctl(fd, VIDIOC_STREAMON, &type))
+        return errnoexit("VIDIOC_STREAMON");
 
     return SUCCESS_LOCAL;
 }
 
-int readframeonce(void)
-{
-    for (;;) {
+int readframeonce(void) {
+    for(;;) {
         fd_set fds;
         struct timeval tv;
         int r;
 
-        FD_ZERO (&fds);
-        FD_SET (fd, &fds);
+        FD_ZERO(&fds);
+        FD_SET(fd, &fds);
 
         tv.tv_sec = 2;
         tv.tv_usec = 0;
 
-        r = select (fd + 1, &fds, NULL, NULL, &tv);
+        r = select(fd + 1, &fds, NULL, NULL, &tv);
 
-        if (-1 == r) {
-            if (EINTR == errno)
+        if(-1 == r) {
+            if(EINTR == errno)
                 continue;
 
-            return errnoexit ("select");
+            return errnoexit("select");
         }
 
-        if (0 == r) {
+        if(0 == r) {
             LOGE("select timeout");
             return ERROR_LOCAL;
 
         }
 
-        if (readframe ()==1)
+        if(readframe()==1)
             break;
 
     }
@@ -255,73 +248,68 @@ int readframeonce(void)
 }
 
 
-void processimage (const void *p)
-{
-        yuyv422toABGRY((unsigned char *)p);
+void processimage(const void *p) {
+    yuyv422toABGRY((unsigned char *)p);
 }
 
-int readframe(void)
-{
+int readframe(void) {
 
     struct v4l2_buffer buf;
     unsigned int i;
 
-    CLEAR (buf);
+    CLEAR(buf);
 
     buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     buf.memory = V4L2_MEMORY_MMAP;
 
-    if (-1 == xioctl (fd, VIDIOC_DQBUF, &buf)) {
-        switch (errno) {
+    if(-1 == xioctl(fd, VIDIOC_DQBUF, &buf)) {
+        switch(errno) {
             case EAGAIN:
                 return 0;
             case EIO:
             default:
-                return errnoexit ("VIDIOC_DQBUF");
+                return errnoexit("VIDIOC_DQBUF");
         }
     }
 
-    assert (buf.index < n_buffers);
+    assert(buf.index < n_buffers);
 
-    processimage (buffers[buf.index].start);
+    processimage(buffers[buf.index].start);
 
-    if (-1 == xioctl (fd, VIDIOC_QBUF, &buf))
-        return errnoexit ("VIDIOC_QBUF");
+    if(-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+        return errnoexit("VIDIOC_QBUF");
 
     return 1;
 }
 
-int stopcapturing(void)
-{
+int stopcapturing(void) {
     enum v4l2_buf_type type;
 
     type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 
-    if (-1 == xioctl (fd, VIDIOC_STREAMOFF, &type))
-        return errnoexit ("VIDIOC_STREAMOFF");
+    if(-1 == xioctl(fd, VIDIOC_STREAMOFF, &type))
+        return errnoexit("VIDIOC_STREAMOFF");
 
     return SUCCESS_LOCAL;
 
 }
 
-int uninitdevice(void)
-{
+int uninitdevice(void) {
     unsigned int i;
 
-    for (i = 0; i < n_buffers; ++i)
-        if (-1 == munmap (buffers[i].start, buffers[i].length))
-            return errnoexit ("munmap");
+    for(i = 0; i < n_buffers; ++i)
+        if(-1 == munmap(buffers[i].start, buffers[i].length))
+            return errnoexit("munmap");
 
-    free (buffers);
+    free(buffers);
 
     return SUCCESS_LOCAL;
 }
 
-int closedevice(void)
-{
-    if (-1 == close (fd)){
+int closedevice(void) {
+    if(-1 == close(fd)) {
         fd = -1;
-        return errnoexit ("close");
+        return errnoexit("close");
     }
 
     fd = -1;
@@ -330,8 +318,7 @@ int closedevice(void)
 
 
 
-void yuyv422toABGRY(unsigned char *src)
-{
+void yuyv422toABGRY(unsigned char *src) {
 
     int width=0;
     int height=0;
@@ -343,7 +330,7 @@ void yuyv422toABGRY(unsigned char *src)
 
     int i;
 
-    if((!rgb || !ybuf)){
+    if((!rgb || !ybuf)) {
         return;
     }
     int *lrgb = NULL;
@@ -352,10 +339,10 @@ void yuyv422toABGRY(unsigned char *src)
     lrgb = &rgb[0];
     lybuf = &ybuf[0];
 
-    if(yuv_tbl_ready==0){
-        for(i=0 ; i<256 ; i++){
+    if(yuv_tbl_ready==0) {
+        for(i=0 ; i<256 ; i++) {
             y1192_tbl[i] = 1192*(i-16);
-            if(y1192_tbl[i]<0){
+            if(y1192_tbl[i]<0) {
                 y1192_tbl[i]=0;
             }
 
@@ -367,7 +354,7 @@ void yuyv422toABGRY(unsigned char *src)
         yuv_tbl_ready=1;
     }
 
-    for(i=0 ; i<frameSize ; i+=4){
+    for(i=0 ; i<frameSize ; i+=4) {
         unsigned char y1, y2, u, v;
         y1 = src[i];
         u = src[i+1];
@@ -375,14 +362,14 @@ void yuyv422toABGRY(unsigned char *src)
         v = src[i+3];
 
         int y1192_1=y1192_tbl[y1];
-        int r1 = (y1192_1 + v1634_tbl[v])>>10;
-        int g1 = (y1192_1 - v833_tbl[v] - u400_tbl[u])>>10;
-        int b1 = (y1192_1 + u2066_tbl[u])>>10;
+        int r1 =(y1192_1 + v1634_tbl[v])>>10;
+        int g1 =(y1192_1 - v833_tbl[v] - u400_tbl[u])>>10;
+        int b1 =(y1192_1 + u2066_tbl[u])>>10;
 
         int y1192_2=y1192_tbl[y2];
-        int r2 = (y1192_2 + v1634_tbl[v])>>10;
-        int g2 = (y1192_2 - v833_tbl[v] - u400_tbl[u])>>10;
-        int b2 = (y1192_2 + u2066_tbl[u])>>10;
+        int r2 =(y1192_2 + v1634_tbl[v])>>10;
+        int g2 =(y1192_2 - v833_tbl[v] - u400_tbl[u])>>10;
+        int b2 =(y1192_2 + u2066_tbl[u])>>10;
 
         r1 = r1>255 ? 255 : r1<0 ? 0 : r1;
         g1 = g1>255 ? 255 : g1<0 ? 0 : g1;
@@ -394,7 +381,7 @@ void yuyv422toABGRY(unsigned char *src)
         *lrgb++ = 0xff000000 | b1<<16 | g1<<8 | r1;
         *lrgb++ = 0xff000000 | b2<<16 | g2<<8 | r2;
 
-        if(lybuf!=NULL){
+        if(lybuf!=NULL) {
             *lybuf++ = y1;
             *lybuf++ = y2;
         }
@@ -403,8 +390,8 @@ void yuyv422toABGRY(unsigned char *src)
 }
 
 
-void
-Java_com_ford_openxc_webcam_CameraPreview_pixeltobmp( JNIEnv* env,jobject thiz,jobject bitmap){
+void Java_com_ford_openxc_webcam_WebcamManager_pixeltobmp( JNIEnv* env,
+        jobject thiz,jobject bitmap) {
 
     jboolean bo;
 
@@ -418,7 +405,7 @@ Java_com_ford_openxc_webcam_CameraPreview_pixeltobmp( JNIEnv* env,jobject thiz,j
     int width=0;
     int height=0;
 
-    if ((ret = AndroidBitmap_getInfo(env, bitmap, &info)) < 0) {
+    if((ret = AndroidBitmap_getInfo(env, bitmap, &info)) < 0) {
         LOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
         return;
     }
@@ -426,22 +413,24 @@ Java_com_ford_openxc_webcam_CameraPreview_pixeltobmp( JNIEnv* env,jobject thiz,j
     width = info.width;
     height = info.height;
 
-    if(!rgb || !ybuf) return;
+    if(!rgb || !ybuf) {
+        return;
+    }
 
-    if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
+    if(info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
         LOGE("Bitmap format is not RGBA_8888 !");
         return;
     }
 
-    if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
+    if((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
         LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
     }
 
-    colors = (int*)pixels;
+    colors =(int*)pixels;
     int *lrgb =NULL;
     lrgb = &rgb[0];
 
-    for(i=0 ; i<width*height ; i++){
+    for(i=0 ; i<width*height ; i++) {
         *colors++ = *lrgb++;
     }
 
@@ -449,72 +438,65 @@ Java_com_ford_openxc_webcam_CameraPreview_pixeltobmp( JNIEnv* env,jobject thiz,j
 
 }
 
-jint
-Java_com_ford_openxc_webcam_CameraPreview_prepareCamera( JNIEnv* env,jobject thiz, jint videoid){
-
+jint Java_com_ford_openxc_webcam_WebcamManager_prepareCamera( JNIEnv* env,
+        jobject thiz, jint videoid) {
     int ret;
 
-    if(camerabase<0){
+    if(camerabase<0) {
         camerabase = checkCamerabase();
     }
 
     ret = opendevice(camerabase + videoid);
 
-    if(ret != ERROR_LOCAL){
+    if(ret != ERROR_LOCAL) {
         ret = initdevice();
     }
-    if(ret != ERROR_LOCAL){
+
+    if(ret != ERROR_LOCAL) {
         ret = startcapturing();
 
-        if(ret != SUCCESS_LOCAL){
+        if(ret != SUCCESS_LOCAL) {
             stopcapturing();
-            uninitdevice ();
-            closedevice ();
+            uninitdevice();
+            closedevice();
             LOGE("device resetted");
         }
 
     }
 
-    if(ret != ERROR_LOCAL){
-        rgb = (int *)malloc(sizeof(int) * (IMG_WIDTH*IMG_HEIGHT));
-        ybuf = (int *)malloc(sizeof(int) * (IMG_WIDTH*IMG_HEIGHT));
+    if(ret != ERROR_LOCAL) {
+        rgb =(int *)malloc(sizeof(int) *(IMG_WIDTH*IMG_HEIGHT));
+        ybuf =(int *)malloc(sizeof(int) *(IMG_WIDTH*IMG_HEIGHT));
     }
     return ret;
 }
 
-
-
-jint
-Java_com_ford_openxc_webcam_CameraPreview_prepareCameraWithBase( JNIEnv* env,jobject thiz, jint videoid, jint videobase){
-
-        int ret;
-
-        camerabase = videobase;
-
-        return Java_com_ford_openxc_webcam_CameraPreview_prepareCamera(env,thiz,videoid);
-
+jint Java_com_ford_openxc_webcam_WebcamManager_prepareCameraWithBase(
+        JNIEnv* env,jobject thiz, jint videoid, jint videobase) {
+    camerabase = videobase;
+    return Java_com_ford_openxc_webcam_WebcamManager_prepareCamera(
+            env, thiz, videoid);
 }
 
-void
-Java_com_ford_openxc_webcam_CameraPreview_processCamera( JNIEnv* env,
-                                        jobject thiz){
-
+void Java_com_ford_openxc_webcam_WebcamManager_processCamera( JNIEnv* env,
+        jobject thiz) {
     readframeonce();
 }
 
-void
-Java_com_ford_openxc_webcam_CameraPreview_stopCamera(JNIEnv* env,jobject thiz){
+void Java_com_ford_openxc_webcam_WebcamManager_stopCamera(JNIEnv* env,
+        jobject thiz) {
+    stopcapturing();
+    uninitdevice();
+    closedevice();
 
-    stopcapturing ();
+    if(rgb) {
+        free(rgb);
+    }
 
-    uninitdevice ();
-
-    closedevice ();
-
-    if(rgb) free(rgb);
-    if(ybuf) free(ybuf);
+    if(ybuf) {
+        free(ybuf);
+    }
 
     fd = -1;
-
 }
 
--- android-webcam-library/jni/ImageProc.h
+++ android-webcam-library/jni/ImageProc.h
@@ -3,32 +3,23 @@
 #include <android/bitmap.h>
 
 #include <string.h>
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <assert.h>
 
-#include <fcntl.h>              /* low-level i/o */
-#include <unistd.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <malloc.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/time.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 
-#include <asm/types.h>          /* for videodev2.h */
-
 #include <linux/videodev2.h>
-#include <linux/usbdevice_fs.h>
 
-#define  LOG_TAG    "WebCam"
-#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define LOG_TAG "WebCam"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
-#define CLEAR(x) memset (&(x), 0, sizeof (x))
+#define CLEAR(x) memset(&(x), 0, sizeof(x))
 
 #define IMG_WIDTH 640
 #define IMG_HEIGHT 480
@@ -37,30 +28,30 @@
 #define SUCCESS_LOCAL 0
 
 struct buffer {
-        void *                  start;
-        size_t                  length;
+    void* start;
+    size_t length;
 };
 
-static char            dev_name[16];
-static int              fd              = -1;
-struct buffer *         buffers         = NULL;
-static unsigned int     n_buffers       = 0;
+static char dev_name[16];
+static int fd = -1;
+struct buffer* buffers = NULL;
+static unsigned int n_buffers = 0;
 
 int camerabase = -1;
 
-int *rgb = NULL;
-int *ybuf = NULL;
+int* rgb = NULL;
+int* ybuf = NULL;
 
-int yuv_tbl_ready=0;
+int yuv_tbl_ready = 0;
 int y1192_tbl[256];
 int v1634_tbl[256];
 int v833_tbl[256];
 int u400_tbl[256];
 int u2066_tbl[256];
 
-int errnoexit(const char *s);
+int errnoexit(const char* s);
 
-int xioctl(int fd, int request, void *arg);
+int xioctl(int fd, int request, void* arg);
 
 int checkCamerabase(void);
 int opendevice(int videoid);
@@ -70,16 +61,21 @@ int startcapturing(void);
 
 int readframeonce(void);
 int readframe(void);
-void processimage (const void *p);
+void processimage(const void* p);
 
 int stopcapturing(void);
 int uninitdevice(void);
 int closedevice(void);
 
-void yuyv422toABGRY(unsigned char *src);
+void yuyv422toABGRY(unsigned char* src);
 
-jint Java_com_ford_openxc_webcam_CameraPreview_prepareCamera( JNIEnv* env,jobject thiz, jint videoid);
-jint Java_com_ford_openxc_webcam_CameraPreview_prepareCameraWithBase( JNIEnv* env,jobject thiz, jint videoid, jint videobase);
-void Java_com_ford_openxc_webcam_CameraPreview_processCamera( JNIEnv* env,jobject thiz);
-void Java_com_ford_openxc_webcam_CameraPreview_stopCamera(JNIEnv* env,jobject thiz);
-void Java_com_ford_openxc_webcam_CameraPreview_pixeltobmp( JNIEnv* env,jobject thiz,jobject bitmap);
+jint Java_com_ford_openxc_webcam_WebcamManager_prepareCamera(JNIEnv* env,
+        jobject thiz, jint videoid);
+jint Java_com_ford_openxc_webcam_WebcamManager_prepareCameraWithBase(
+        JNIEnv* env, jobject thiz, jint videoid, jint videobase);
+void Java_com_ford_openxc_webcam_WebcamManager_processCamera(JNIEnv* env,
+        jobject thiz);
+void Java_com_ford_openxc_webcam_WebcamManager_stopCamera(JNIEnv* env,
+        jobject thiz);
+void Java_com_ford_openxc_webcam_WebcamManager_pixeltobmp(JNIEnv* env,
+        jobject thiz, jobject bitmap);
