2c0a0e49928f44131cd0a0d3c7fe6b8fc5a87e52
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-02-26 20:33:30 -0500

    Organize and document all JNI functions.

--- android-webcam-library/jni/Android.mk
+++ android-webcam-library/jni/Android.mk
@@ -17,7 +17,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := webcam
-LOCAL_SRC_FILES := webcam.c
+LOCAL_SRC_FILES := webcam.c yuv.c video_device.c util.c capture.c
 LOCAL_LDLIBS    := -llog -ljnigraphics
 LOCAL_CFLAGS += -std=c99
 
--- android-webcam-library/jni/webcam.h
+++ android-webcam-library/jni/webcam.h
@@ -1,43 +1,15 @@
+#ifndef __WEBCAM_H__
+#define __WEBCAM_H__
+
 #include <jni.h>
-#include <android/log.h>
-#include <android/bitmap.h>
 
-#include <string.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <assert.h>
-
-#include <fcntl.h>
-#include <errno.h>
-#include <malloc.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <sys/ioctl.h>
-
-#include <linux/videodev2.h>
-
-#define LOG_TAG "NativeWebcamJNI"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
-
-#define CLEAR(x) memset(&(x), 0, sizeof(x))
-
-#define ERROR_LOCAL -1
-#define SUCCESS_LOCAL 0
-
-typedef struct {
-    void* start;
-    size_t length;
-} buffer;
+#include "util.h"
 
 static int DEVICE_DESCRIPTOR = -1;
 int* RGB_BUFFER = NULL;
 int* Y_BUFFER = NULL;
-buffer* FRAME_BUFFERS = NULL;
-static unsigned int BUFFER_COUNT = 0;
 
-int YUV_TABLE[5][256];
-
+// These are documented on the Java side, in NativeWebcam
 jint Java_com_ford_openxc_webcam_NativeWebcam_startCamera(JNIEnv* env,
         jobject thiz, jstring deviceName, jint width, jint height);
 void Java_com_ford_openxc_webcam_NativeWebcam_loadNextFrame(JNIEnv* env,
@@ -46,3 +18,5 @@ jboolean Java_com_ford_openxc_webcam_NativeWebcam_cameraAttached(JNIEnv* env,
         jobject thiz);
 void Java_com_ford_openxc_webcam_NativeWebcam_stopCamera(JNIEnv* env,
         jobject thiz);
+
+#endif // __WEBCAM_H__
